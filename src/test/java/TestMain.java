3f939721cdf3eff5b8263a3b14d63995728fb639
Alexey Astafev <31274867+astalexey@users.noreply.github.com>
2022-12-20 13:28:29 +0400

    qeqeq

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -5,7 +5,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TestMain {
 
     @Test
-    public void testInputIsEven(){
-        adfgsdfgsdfgsdfhsfdhdshf // Assertion
+   public void TestUntitled2() throws Exception {
+    driver.get(baseUrl);
+    driver.findElement(By.linkText("Войти")).click();
+    driver.findElement(By.linkText("Войти")).click();
+    driver.findElement(By.id("login")).clear();
+    driver.findElement(By.id("login")).sendKeys("Selenium");
+    driver.findElement(By.id("password")).clear();
+    driver.findElement(By.id("password")).sendKeys("123456");
+    driver.findElement(By.xpath("//*[@id='form']/a[1]")).click();
+    driver.findElement(By.linkText("Пополнить счёт")).click();
+    driver.findElement(By.id("amount")).clear();
+    driver.findElement(By.id("amount")).sendKeys("10");
+    driver.findElement(By.linkText("Электронные деньги")).click();
+    driver.findElement(By.id("mode_type_10")).click();
+    driver.findElement(By.id("send_message")).click();
+    driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    Assert.assertTrue(isElementPresent(By.id("auth")));
+    
     }
 }
