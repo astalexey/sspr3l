5b2360c0a0bdea0b86129312866f2e7c4e4fe2f1
Alexey Astafev <31274867+astalexey@users.noreply.github.com>
2022-12-20 13:18:12 +0400

    1241243cas

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -6,6 +6,6 @@ public class TestMain {
 
     @Test
     public void testInputIsEven(){
-        assertTrue(Main.checkIfInputIsAnEvenNumber(3), false); // Assertion
+        assertEquals(Main.checkIfInputIsAnEvenNumber(3), false); // Assertion
     }
 }
